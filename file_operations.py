2248362ced4564aedb63f9a8743f07d240b7c87f
Vijay <vijaykumarrdhanapal@gmail.com>
2024-03-19 22:18:44 +0530

    postal function calling changed in file operation.py

--- file_operations.py
+++ file_operations.py
@@ -14,6 +14,17 @@ def read_config():
 column_data_types = read_config()
 
 
+def load_postal_code_df():
+    print("entering load_postal_code_df")
+    postal_code_file_uri = CONFIG_JSON["GCS_BUCKET_PATH"]["POSTAL_BUCKET_LOCATION"]
+    df = pd.read_csv(postal_code_file_uri)
+    df_post = pd.DataFrame().assign(pincode=df['pincode'], city=df['regionname'])
+    postal_df = df_post.drop_duplicates(subset=['pincode'], keep='first')
+    print("end load_postal_code_df")
+    print(postal_df.head(5))
+    return postal_df
+
+
 class ShopperFile:
     __client = bigquery.Client()
     __project_id = CONFIG_JSON["GOOGLE_CONFIG"]["PROJECT_ID"]
@@ -31,27 +42,39 @@ class ShopperFile:
         self.prd_bucket_name = CONFIG_JSON["GCS_BUCKET_PATH"]["PRD_BUCKET_NAME"]
 
     def load_content(self):
+        print("entering load_content")
         excel_file_uri = f"gs://{self.excel_bucket_name}/{self.excel_file_name}"
+        print(excel_file_uri)
+        print("entering second line")
         with self.fs.open(excel_file_uri) as file:
+            print("entering third line")
             df = pd.read_excel(file, engine='openpyxl')
+            print("entering fourth line")
+            print(df)
+        print("entering fifth line")
+        print(df)
+        print("end load_content")
         return df
 
     @staticmethod
     def clean_sex_column(value):
+        print("entering clean_sex_column")
         if pd.isna(value):
             return value
+
+        male_pattern = re.compile(r'male', re.IGNORECASE)
+        female_pattern = re.compile(r'female', re.IGNORECASE)
+        if female_pattern.search(value):
+            return "female"
+        elif value != "female":
+            male_pattern.search(value)
+            return "male"
         else:
-            male_pattern = re.compile(r'male', re.IGNORECASE)
-            female_pattern = re.compile(r'female', re.IGNORECASE)
-            if female_pattern.search(value):
-                return "female"
-            elif value != "female":
-                male_pattern.search(value)
-                return "male"
-            else:
-                return pd.NA
+            return pd.NA
+        print("end clean_sex_column")
 
     def fix_column_data_types(self):
+        print("entering fix_column_data_types")
         df = self.load_content()
         for col, expected_dtype in column_data_types.items():
             if expected_dtype == 'str':
@@ -61,18 +84,30 @@ class ShopperFile:
                                                                                                      errors='ignore')
         df['sex'] = df['sex'].apply(self.clean_sex_column)
         print(df)
+        print("end fix_column_data_types")
         return df
 
+    def city_column(self):
+        print("entering city_column")
+        df_main = self.fix_column_data_types()
+        df_clean = pd.merge(df_main, load_postal_code_df(), how='left', left_on='pincode', right_on='pincode')
+        print(df_clean.head(5))
+        print("end city_column")
+        return df_clean
+
     def export_csv_to_gcs(self, gcs_bucket_location=None, bucket_name=None):
-        df = self.fix_column_data_types()
+        print("entering export_csv_to_gcs")
+        df = self.city_column()
         bucket_files = list(self.fs.ls(bucket_name))
         if bucket_files:
             for filepath in bucket_files:
                 self.fs.rm(filepath, recursive=True)
         with self.fs.open(gcs_bucket_location, 'w') as f:
             df.to_csv(f, index=False)
+        print("end export_csv_to_gcs")
 
     def validate_external_table(self,table_name=None):
+        print("entering validate_external_table")
         select_query = f"""select * from {self.__project_id}.{self.__db_name}.{table_name};"""
         is_output_empty_or_error = True
         try:
@@ -89,11 +124,14 @@ class ShopperFile:
         return is_output_empty_or_error
 
     def test_deploy(self):
+        print("entering test_deploy")
         self.export_csv_to_gcs(gcs_bucket_location=self.dev_bucket_location, bucket_name=self.dev_bucket_name)
         is_output_empty_or_error = self.validate_external_table(table_name=self.__dev_table_name)
+        print("end test_deploy")
         return is_output_empty_or_error
 
     def production_deploy(self):
+        print("entering production_deploy")
         is_dev_output_empty_or_error = self.test_deploy()
         if is_dev_output_empty_or_error:
             print("The output is empty or an error occurred in development phase.")
@@ -102,6 +140,7 @@ class ShopperFile:
             is_prd_output_empty_or_error = self.validate_external_table(table_name=self.__prd_table_name)
             if is_prd_output_empty_or_error:
                 print("The output is empty or an error occurred in production stage.")
+        print("end production_deploy")
 
 
 
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ tabel_schema_json = read_config()
 
 
 def hello_gcs(event, context):
+    print("entering gcs function")
     excel_bucket_name = event['bucket']
     excel_file_name = event['name']
     store = file_operations.ShopperFile(excel_file_name, excel_bucket_name)
