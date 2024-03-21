9bc88f43edae4bc89fd2090843e9689e33b76251
Vijay <vijaykumarrdhanapal@gmail.com>
2024-03-21 11:21:05 +0530

    logging added

--- file_operations.py
+++ file_operations.py
@@ -15,12 +15,13 @@ column_data_types = read_config()
 
 
 def load_postal_code_df():
-    print("entering load_postal_code_df")
+    print("Loading postal code details...")
     postal_code_file_uri = CONFIG_JSON["GCS_BUCKET_PATH"]["POSTAL_BUCKET_LOCATION"]
     df = pd.read_csv(postal_code_file_uri)
     df_post = pd.DataFrame().assign(pincode=df['pincode'], city=df['regionname'])
     postal_df = df_post.drop_duplicates(subset=['pincode'], keep='first')
-    print("end load_postal_code_df")
+    print("Postal code details loaded successfully")
+    print("Sample top records from 'postal_df':")
     print(postal_df.head(5))
     return postal_df
 
@@ -33,6 +34,7 @@ class ShopperFile:
     __prd_table_name = CONFIG_JSON["GOOGLE_CONFIG"]["PRD_TABLE_NAME"]
 
     def __init__(self,excel_file_name,excel_bucket_name):
+        print("Initializing ShopperFile instance...")
         self.excel_file_name = excel_file_name
         self.excel_bucket_name = excel_bucket_name
         self.dev_bucket_location = CONFIG_JSON["GCS_BUCKET_PATH"]["DEV_BUCKET_LOCATION"]
@@ -42,23 +44,18 @@ class ShopperFile:
         self.prd_bucket_name = CONFIG_JSON["GCS_BUCKET_PATH"]["PRD_BUCKET_NAME"]
 
     def load_content(self):
-        print("entering load_content")
+        print("Loading content from Excel file...")
         excel_file_uri = f"gs://{self.excel_bucket_name}/{self.excel_file_name}"
-        print(excel_file_uri)
-        print("entering second line")
+        print(f"Accessing Excel file at URI: {excel_file_uri}")
         with self.fs.open(excel_file_uri) as file:
-            print("entering third line")
             df = pd.read_excel(file, engine='openpyxl')
-            print("entering fourth line")
-            print(df)
-        print("entering fifth line")
-        print(df)
-        print("end load_content")
+        print("printing excel file dataframe sample 3 records",df.head(3))
+        print("Content loaded successfully")
         return df
 
     @staticmethod
     def clean_sex_column(value):
-        print("entering clean_sex_column")
+        print("Cleaning 'sex' column values...")
         if pd.isna(value):
             return value
 
@@ -71,10 +68,10 @@ class ShopperFile:
             return "male"
         else:
             return pd.NA
-        print("end clean_sex_column")
+
 
     def fix_column_data_types(self):
-        print("entering fix_column_data_types")
+        print("Fixing column data types...")
         df = self.load_content()
         for col, expected_dtype in column_data_types.items():
             if expected_dtype == 'str':
@@ -83,20 +80,20 @@ class ShopperFile:
                 df[col] = df[col].apply(lambda x: x if isinstance(x, (int, float)) else pd.NA).astype('Int64',
                                                                                                      errors='ignore')
         df['sex'] = df['sex'].apply(self.clean_sex_column)
-        print(df)
-        print("end fix_column_data_types")
+        print("Sample top records from 'fix_column_data_types':",df.head(3))
+        print("Column data types adjusted")
         return df
 
     def city_column(self):
-        print("entering city_column")
+        print("Merging city data with postal codes information...")
         df_main = self.fix_column_data_types()
         df_clean = pd.merge(df_main, load_postal_code_df(), how='left', left_on='pincode', right_on='pincode')
-        print(df_clean.head(5))
-        print("end city_column")
+        print("Sample top records from 'df_clean':")
+        print(df_clean.head(3))
         return df_clean
 
     def export_csv_to_gcs(self, gcs_bucket_location=None, bucket_name=None):
-        print("entering export_csv_to_gcs")
+        print("Exporting DataFrame to CSV file in GCS...")
         df = self.city_column()
         bucket_files = list(self.fs.ls(bucket_name))
         if bucket_files:
@@ -104,10 +101,10 @@ class ShopperFile:
                 self.fs.rm(filepath, recursive=True)
         with self.fs.open(gcs_bucket_location, 'w') as f:
             df.to_csv(f, index=False)
-        print("end export_csv_to_gcs")
+        print("Successfully exported DataFrame to CSV in GCS")
 
     def validate_external_table(self,table_name=None):
-        print("entering validate_external_table")
+        print("validating external table" )
         select_query = f"""select * from {self.__project_id}.{self.__db_name}.{table_name};"""
         is_output_empty_or_error = True
         try:
@@ -124,14 +121,14 @@ class ShopperFile:
         return is_output_empty_or_error
 
     def test_deploy(self):
-        print("entering test_deploy")
+        print("Completed test deployment")
         self.export_csv_to_gcs(gcs_bucket_location=self.dev_bucket_location, bucket_name=self.dev_bucket_name)
         is_output_empty_or_error = self.validate_external_table(table_name=self.__dev_table_name)
-        print("end test_deploy")
+        print("Completed test deployment")
         return is_output_empty_or_error
 
     def production_deploy(self):
-        print("entering production_deploy")
+        print("Initiating production deployment process...")
         is_dev_output_empty_or_error = self.test_deploy()
         if is_dev_output_empty_or_error:
             print("The output is empty or an error occurred in development phase.")
@@ -140,7 +137,7 @@ class ShopperFile:
             is_prd_output_empty_or_error = self.validate_external_table(table_name=self.__prd_table_name)
             if is_prd_output_empty_or_error:
                 print("The output is empty or an error occurred in production stage.")
-        print("end production_deploy")
+        print("Production deployment process completed successfully")
 
 
 
--- main.py
+++ main.py
@@ -11,8 +11,9 @@ tabel_schema_json = read_config()
 
 
 def hello_gcs(event, context):
-    print("entering gcs function")
+    print("Processing Google Cloud Storage event...")
     excel_bucket_name = event['bucket']
     excel_file_name = event['name']
+    print(f"Received Excel file: {excel_file_name} from bucket: {excel_bucket_name}")
     store = file_operations.ShopperFile(excel_file_name, excel_bucket_name)
     store.production_deploy()
